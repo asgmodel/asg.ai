d81983b270550298c1ce9725ede9eed621c2363b
asgmodel <145135565+asgmodel@users.noreply.github.com>
2025-02-26 21:51:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Optional
 import gradio as gr
 
 from fastapi import FastAPI
-import dash
+import dashborad
 app = FastAPI()
 
 
@@ -15,4 +15,4 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-app = gr.mount_gradio_app(app, dash.demo, path='/dash')
+app = gr.mount_gradio_app(app, dashborad.demo, path='/dash')
