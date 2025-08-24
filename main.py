9776832911fb7e35b84334231f0444d997d695ad
Anas Al-Tawil <145135565+asgmodel@users.noreply.github.com>
2025-08-24 18:16:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 import gradio as gr
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 import dash
@@ -14,5 +15,23 @@ async def root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+# تهيئة Gradio Client مرة واحدة
 
+# نموذج للطلب
+class PromptRequest(BaseModel):
+    prompt: str
+
+# endpoint رئيسي للتوليد
+@app.post("/generate")
+def generate_text(req: PromptRequest):
+    try:
+        client = Client("wasmdashai/LAHJA-AI")
+
+        result = client.predict(
+            prompt=req.prompt,
+            api_name="/generate_from_prompt"
+        )
+        return {"input": req.prompt, "output": result}
+    except Exception as e:
+        return {"error": str(e)}
 app = gr.mount_gradio_app(app, dash.demo, path='/dash')
